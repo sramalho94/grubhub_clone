df1d72a586dd9a0ab0719386c80daca1cdf1d5b9
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-12 10:32:16 -0400

    configured tailwind for project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './screens/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './App.{js,jsx,ts,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
-
