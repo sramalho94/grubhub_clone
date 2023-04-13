00f0126fc7a5798b75db0ce3c22f8f5b3a38fdde
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-13 11:19:15 -0400

    added react-native-heroicons imports

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,12 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon
+} from 'react-native-heroicons/outline'
 const HomeScreen = () => {
   // get navigation object from useNavigation hook
   const navigation = useNavigation()
@@ -16,7 +21,25 @@ const HomeScreen = () => {
   return (
     // screen is loaded within Safe Area View so it doesn't clip into uphone notch area
     <SafeAreaView>
-      <Text className="text-red-500">Home Screen</Text>
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pn-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: 'https://links.papareact.com/wru'
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+        </View>
+      </Text>
     </SafeAreaView>
   )
 }
