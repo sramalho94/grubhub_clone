dd1f2ed72f477a7d197af288626b57877ecc1b9f
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-13 12:15:19 -0400

    created navbar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,13 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import {
   UserIcon,
   ChevronDownIcon,
-  SearchIcon,
-  AdjustmentsIcon
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon
 } from 'react-native-heroicons/outline'
+
 const HomeScreen = () => {
   // get navigation object from useNavigation hook
   const navigation = useNavigation()
@@ -20,26 +21,39 @@ const HomeScreen = () => {
 
   return (
     // screen is loaded within Safe Area View so it doesn't clip into uphone notch area
-    <SafeAreaView>
-      <Text className="text-red-500">
-        {/* Header */}
-        <View className="flex-row pn-3 items-center mx-4 space-x-2">
-          <Image
-            source={{
-              uri: 'https://links.papareact.com/wru'
-            }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-          />
+    <SafeAreaView className="bg-white pt-5">
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-2">
+        <Image
+          source={{
+            uri: 'https://links.papareact.com/wru'
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
 
-          <View>
-            <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
-            <Text className="font-bold text-xl">
-              Current Location
-              <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
         </View>
-      </Text>
+
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="search by restaurant or food"
+            keyboardType="default"
+          />
+        </View>
+
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
     </SafeAreaView>
   )
 }
