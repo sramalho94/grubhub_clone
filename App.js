51215857bea4e7c76963a8c85134c29bbd4d45f5
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-13 10:50:48 -0400

    incorporated React Navigation to App.js and set screen to Homescreen

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './screens/HomeScreen'
 
+const Stack = createNativeStackNavigator()
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-bold text-3xl">
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* Screens here */}
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
