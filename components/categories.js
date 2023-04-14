9130293388c92deec15acb9bbca917f74b66fa30
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-14 11:47:59 -0400

    added more category cards to test scrollview

--- components/categories.js
+++ components/categories.js
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import CategoryCard from './CategoryCard'
 
-const categories = () => {
+const Categories = () => {
   return (
-    <View>
-      <Text>categories</Text>
-    </View>
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ paddingHorizontal: 15, paddingTop: 10 }}
+    >
+      {/* Category Card */}
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 1"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 2"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 3"
+      />
+      <CategoryCard
+        imgUrl="https://links.papareact.com/gn7"
+        title="Testing 3"
+      />
+    </ScrollView>
   )
 }
 
-export default categories
+export default Categories
