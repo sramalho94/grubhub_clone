b4484002c5e6f438eb61bf48375e18d6732acbae
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-14 12:24:34 -0400

    added more restaurantcards to test scrollview

--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -1,6 +1,7 @@
 import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import { ArrowRightIcon } from 'react-native-heroicons/outline'
+import RestaurantCard from './RestaurantCard'
 
 const FeaturedRow = ({ id, title, description }) => {
   return (
@@ -20,6 +21,42 @@ const FeaturedRow = ({ id, title, description }) => {
         }}
       >
         {/* Restaurant Cards */}
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yo! Sushi"
+          rating={4.5}
+          genre="japanese"
+          address="123 Main St"
+          short_description="This is a Test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yo! Sushi"
+          rating={4.5}
+          genre="japanese"
+          address="123 Main St"
+          short_description="This is a Test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
+        <RestaurantCard
+          id={123}
+          imgUrl="https://links.papareact.com/gn7"
+          title="Yo! Sushi"
+          rating={4.5}
+          genre="japanese"
+          address="123 Main St"
+          short_description="This is a Test description"
+          dishes={[]}
+          long={20}
+          lat={0}
+        />
       </ScrollView>
     </View>
   )
