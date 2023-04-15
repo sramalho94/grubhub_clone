0a0b4f2d722818e0674e1022595e860d215ec36f
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-15 16:58:44 -0400

    created schema for restaurant

--- sanity/schemas/restaurant.js
+++ sanity/schemas/restaurant.js
@@ -45,5 +45,19 @@ export default defineType({
       validation: (Rule) =>
         Rule.required().min(1).max(5).error('Please enter a value between 1 and 5'),
     },
+    {
+      name: 'type',
+      type: 'string',
+      title: 'Category',
+      validation: (Rule) => Rule.required(),
+      type: 'reference',
+      to: [{type: 'category'}],
+    },
+    {
+      name: 'dishes',
+      type: 'array',
+      title: 'Dishes',
+      of: [{type: 'reference', to: [{type: 'dish'}]}],
+    },
   ],
 })
