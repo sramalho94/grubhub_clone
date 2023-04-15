4067838830f49dba54069fc41ef84d39f4372a1e
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-15 16:57:02 -0400

    modified category to fit project

--- sanity/schemas/category.js
+++ sanity/schemas/category.js
@@ -2,18 +2,10 @@ import {defineField, defineType} from 'sanity'
 
 export default defineType({
   name: 'category',
-  title: 'Category',
+  title: 'Menu Category',
   type: 'document',
   fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    }),
+    {name: 'name', type: 'string', title: 'Category name', validation: (Rule) => Rule.required()},
+    {name: 'image', type: 'image', title: 'Image of Category'},
   ],
 })
