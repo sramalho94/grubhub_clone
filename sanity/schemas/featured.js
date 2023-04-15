cc91aaacf02dd298fe2e1b2d1fb41a38ff0688d2
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-15 16:58:18 -0400

    added schema for featured category

---
+++ sanity/schemas/featured.js
@@ -0,0 +1,27 @@
+import {defineField, defineType} from 'sanity'
+
+export default {
+  name: 'featured',
+  type: 'document',
+  title: 'Featured Menu categories',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Featured category name',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'short_description',
+      type: 'string',
+      title: 'Short Description',
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: 'restaurants',
+      type: 'array',
+      title: 'Restaurants',
+      of: [{type: 'reference', to: [{type: 'restaurant'}]}],
+    },
+  ],
+}
