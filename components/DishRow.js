596d0adc4023f51e5c4ca6a97d1fc33670c5c3f1
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-15 22:05:03 -0400

    created DishRow component

---
+++ components/DishRow.js
@@ -0,0 +1,12 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const DishRow = ({ id, name, description, price, image }) => {
+  return (
+    <TouchableOpacity>
+      <Text>DishRow</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default DishRow
