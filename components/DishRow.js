f27d0dee7516a6dcd3d1b97f02382f3fd2d060b6
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-16 11:43:58 -0400

    added conditional color to minus button based on amount of items

--- components/DishRow.js
+++ components/DishRow.js
@@ -58,7 +58,10 @@ const DishRow = ({ id, name, description, price, image }) => {
         <View className="bg-white px-4">
           <View className="flex-row items-center space-x-2 pb-3">
             <TouchableOpacity onPress={removeItemFromBasket}>
-              <MinusCircleIcon color="#00CCBB" size={40} />
+              <MinusCircleIcon
+                color={items.length > 0 ? '#00CCBB' : 'gray'}
+                size={40}
+              />
             </TouchableOpacity>
 
             <Text>{itemCount}</Text>
