4bcdf4f7611c7eefcbd20175ec17298261580d65
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-16 11:45:07 -0400

    minus button disabled if no items

--- components/DishRow.js
+++ components/DishRow.js
@@ -57,7 +57,10 @@ const DishRow = ({ id, name, description, price, image }) => {
       {isPressed && (
         <View className="bg-white px-4">
           <View className="flex-row items-center space-x-2 pb-3">
-            <TouchableOpacity onPress={removeItemFromBasket}>
+            <TouchableOpacity
+              onPress={removeItemFromBasket}
+              disabled={!items.length}
+            >
               <MinusCircleIcon
                 color={items.length > 0 ? '#00CCBB' : 'gray'}
                 size={40}
