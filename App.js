76e654ae20b9c784ae50e4b4503d6e7147267d80
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-16 10:39:57 -0400

    incorporating redux into project

--- App.js
+++ App.js
@@ -5,16 +5,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from './screens/HomeScreen'
 import RestaurantScreen from './screens/RestaurantScreen'
 import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 const Stack = createNativeStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {/* Screens here */}
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-      </Stack.Navigator>
+      <Provider store={store}>
+        <Stack.Navigator>
+          {/* Screens here */}
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+        </Stack.Navigator>
+      </Provider>
     </NavigationContainer>
   )
 }
