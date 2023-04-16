6b7e48f2e319e7fd0aea3655a31fd64dae91acc0
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-15 22:51:07 -0400

    tweaks to props

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,10 +1,21 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import Currency from 'react-currency-formatter'
+import { urlFor } from '../sanity'
 
 const DishRow = ({ id, name, description, price, image }) => {
   return (
     <TouchableOpacity>
-      <Text>DishRow</Text>
+      <View>
+        <Text className="text-lg mb-1">{name}</Text>
+        <Text className="text-gray-400">{description}</Text>
+        <Text className="text-gray-400">
+          <Currency quantity={price} currency="USD" />
+        </Text>
+      </View>
+      <View>
+        <Image source={{ uri: image }} className="h-20 w-20 bg-gray-300 p-4" />
+      </View>
     </TouchableOpacity>
   )
 }
