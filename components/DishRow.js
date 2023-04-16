780560a6777967cd25ad388986e1d8b3970f0ad3
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-16 09:58:37 -0400

    finished building out and styling component

--- components/DishRow.js
+++ components/DishRow.js
@@ -5,16 +5,23 @@ import { urlFor } from '../sanity'
 
 const DishRow = ({ id, name, description, price, image }) => {
   return (
-    <TouchableOpacity>
-      <View>
-        <Text className="text-lg mb-1">{name}</Text>
-        <Text className="text-gray-400">{description}</Text>
-        <Text className="text-gray-400">
-          <Currency quantity={price} currency="USD" />
-        </Text>
-      </View>
-      <View>
-        <Image source={{ uri: image }} className="h-20 w-20 bg-gray-300 p-4" />
+    <TouchableOpacity className="bg-white border p-4 border-x-gray-200">
+      <View className="flex-row">
+        <View className="flex-1 pr-2">
+          <Text className="text-lg mb-1">{name}</Text>
+          <Text className="text-gray-400">{description}</Text>
+          <Text className="text-gray-400">
+            <Currency quantity={price} currency="USD" />
+          </Text>
+        </View>
+
+        <View>
+          <Image
+            source={{ uri: image }}
+            className="h-20 w-20 bg-gray-300 p-4"
+            style={{ borderWidth: 1, borderColor: '#F3F3F4' }}
+          />
+        </View>
       </View>
     </TouchableOpacity>
   )
