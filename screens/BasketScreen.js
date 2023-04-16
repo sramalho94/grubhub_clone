a5b54410583941230bee0a336e633ca82954c2e9
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-16 13:06:11 -0400

    building out BasketScreen

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,9 +1,10 @@
-import { Text, View } from 'react-native'
-import React, { Component, useState } from 'react'
+import { SafeAreaView, Text, View, TouchableOpacity } from 'react-native'
+import React, { Component, useState, useMemo } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { selectRestaurant } from '../features/restaurantSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectBasketItems } from '../features/basketSlice'
+import { XCircleIcon } from 'react-native-heroicons/outline'
 
 const BasketScreen = () => {
   const navigation = useNavigation()
@@ -13,12 +14,34 @@ const BasketScreen = () => {
   const dispatch = useDispatch()
 
   // ensuring that call only happens when items change
-  useMemo(() => {}, [items])
+  useMemo(() => {
+    const groupedItems = items.reduce((results, item) => {
+      ;(results[items.id] = results[items.id] || []).push(item)
+      return results
+    }, {})
+
+    setGroupedItemsInBasket(groupedItems)
+  }, [items])
+
+  console.log(groupedItemsInBasket)
 
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <View>
+        <View>
+          <View>
+            <Text className="text-lg font-bold text-center">Basket</Text>
+            <Text className="text-center text-gray-400">
+              {restaurant.title}
+            </Text>
+          </View>
+
+          <TouchableOpacity onPress={navigation.goBack}>
+            <XCircleIcon color="#00CCBB" height={50} width={50} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
