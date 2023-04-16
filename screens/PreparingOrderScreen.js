a8fe8d6ae6680ea58647ca29625303b53b550d29
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-16 14:14:55 -0400

    added animations and text

--- screens/PreparingOrderScreen.js
+++ screens/PreparingOrderScreen.js
@@ -1,10 +1,24 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
+import * as Animatable from 'react-native-animatable'
 
 const PreparingOrderScreen = () => {
   return (
     <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center">
-      <Text>PerparingOrderScreen</Text>
+      <Animatable.Image
+        source={require('../assets/loading.gif')}
+        animation="slideInUp"
+        iterationCount={1}
+        className="h-96 w-96"
+      />
+
+      <Animatable.Text
+        animation="slideInUp"
+        iterationCount={1}
+        className="text-lg my-10 text-white font-bold text-center"
+      >
+        Waiting for Restaurant to accept your order!
+      </Animatable.Text>
     </SafeAreaView>
   )
 }
