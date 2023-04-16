02d5a326c9c91417e5d1f3f0dff3ee67f9e2c98a
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-16 14:03:17 -0400

    button now goes to PreparingOrderScreen

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -111,7 +111,10 @@ const BasketScreen = () => {
             <Text className="text-black font-extrabold">{`$${finalCost}`}</Text>
           </View>
 
-          <TouchableOpacity className="rounded-lg bg-[#00CCBB] p-4">
+          <TouchableOpacity
+            onPress={navigation.navigate('PreparingOrderScreen')}
+            className="rounded-lg bg-[#00CCBB] p-4"
+          >
             <Text className="text-center text-white text-lg font-bold">
               Place Order
             </Text>
